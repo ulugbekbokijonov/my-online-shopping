6e9c4861e1d191ec1abe8382b7e998a10be594ef
ulugbekbokijonov <bokijonovulugbek@gmail.com>
2021-02-18 11:19:33 +0500

    added files to homepag

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Homepage from './homepage.component';
+import {Switch, Route} from 'react-router-dom'
+
+
+const HomePage = (props)=> {
+  console.log(props);
+  return(
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+const TopicsList = ()=> {
+  return(
+    <div>
+      <h1>Topiclist  Page</h1>
+    </div>
+  )
+}
+
+const TopicDetail = ()=> {
+  return(
+    <div>
+      <h1>TopicDeatil  Page</h1>
+    </div>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return  (
+
+        <div > 
+                <Switch>
+                   <Route exact path='/' component={HomePage} /> 
+                   <Route  exact path='/topics' component={TopicsList} /> 
+                   <Route path='/topics/:topicId' component={TopicDetail} /> 
+                </Switch>
+               
+        </div>
+  ) 
+  
+  
+  
 }
 
 export default App;
